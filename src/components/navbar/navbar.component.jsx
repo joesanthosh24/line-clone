c8a26e2f342cd3a0e58f90f875a4dfefc801a3db
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-04-13 11:01:19 -0400

    Added a hamburger to navbar which will appear when resized to a much smaller window size and the different screen sections Life on Line, Messages and Services will appear there

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import LanguageIcon from "@material-ui/icons/Language";
@@ -5,32 +6,63 @@ import LanguageIcon from "@material-ui/icons/Language";
 import classes from "./navbar.module.css";
 
 const Navbar = ({ selected }) => {
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  useEffect(() => {
+    setDropDownOpen(false);
+  }, [selected]);
+
   return (
-    <div className={classes.navbar}>
-      <div className={classes.homeText}>
-        <Link to="/">
-          <p>LINE</p>
-        </Link>
+    <>
+      <div className={classes.navbar}>
+        <div className={classes.homeText}>
+          <Link to="/">
+            <p>LINE</p>
+          </Link>
+        </div>
+        <div className={classes.navContent}>
+          <Link to="/">
+            <p className={selected === "/" && classes.active}>Life on Line</p>
+          </Link>
+          <Link to="/messages">
+            <p className={selected === "/messages" && classes.active}>
+              Messenger App
+            </p>
+          </Link>
+          <Link to="/services">
+            <p className={selected === "/services" && classes.active}>
+              Services
+            </p>
+          </Link>
+        </div>
+        <div className={classes.language}>
+          <LanguageIcon />
+          <p>EN</p>
+          <ArrowDropDownIcon />
+        </div>
+        <div
+          className={classes.hamburger}
+          onClick={() => setDropDownOpen(!dropDownOpen)}
+        >
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+        </div>
       </div>
-      <div className={classes.navContent}>
-        <Link to="/">
-          <p className={selected === "/" && classes.active}>Life on Line</p>
-        </Link>
-        <Link to="/messages">
-          <p className={selected === "/messages" && classes.active}>
-            Messenger App
-          </p>
-        </Link>
-        <Link to="/services">
-          <p className={selected === "/services" && classes.active}>Services</p>
-        </Link>
-      </div>
-      <div className={classes.language}>
-        <LanguageIcon />
-        <p>EN</p>
-        <ArrowDropDownIcon />
-      </div>
-    </div>
+      {dropDownOpen && (
+        <div className={classes.navContentDropDown}>
+          <Link to="/">
+            <p>Life on Line</p>
+          </Link>
+          <Link to="/messages">
+            <p>Messenger App</p>
+          </Link>
+          <Link to="/services">
+            <p>Services</p>
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 
